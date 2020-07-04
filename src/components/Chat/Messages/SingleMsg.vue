70589c094798520bde6b6b6f2df92ba43a085d79
Mirko Rajovic <rajovicmirko@gmail.com>
2020-07-04 20:54:22 +0200

    paragraf width for message fixed

--- src/components/Chat/Messages/SingleMsg.vue
+++ src/components/Chat/Messages/SingleMsg.vue
@@ -127,7 +127,7 @@ $border: 3px solid change-color($secondary, $alpha: 0.5);
       white-space: pre-wrap;
       word-wrap: break-word;
       width: 100%;
-      min-width: 254px;
+      min-width: 220px;
     }
 
   }
